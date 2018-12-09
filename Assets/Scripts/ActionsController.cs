35da0a236a03de54d0dbb05128ee2421297051bc
Omar Doma <omar.elias.96@gmail.com>
2018-12-09 20:32:42 +0200

    Sounds

---
+++ Assets/Scripts/ActionsController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ActionsController : MonoBehaviour
+{
+    public void LoadByIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
---
+++ Assets/Scripts/SoundController.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+public class SoundController : MonoBehaviour
+{
+    private static SoundController instance;
+    private float speechVolume;
+    private float sfxVolume;
+    private float musicVolume;
+    private int currentMusicIndex;
+    public AudioSource source;
+    public AudioClip[] musicClips;
+
+    public static SoundController Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void Start()
+    {
+        PlayMusic(0);
+    }
+
+    public void PauseMusic()
+    {
+        source.Pause();
+    }
+
+    public void ResumeMusic()
+    {
+        source.UnPause();
+    }
+
+    public void PlayMusic(int index)
+    {
+        if (source.isPlaying)
+        {
+            source.Stop();
+        }
+        currentMusicIndex = index % musicClips.Length;
+        source.clip = musicClips[currentMusicIndex];
+        source.Play();
+    }
+}
